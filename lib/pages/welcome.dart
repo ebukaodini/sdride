054ad19274523bb754227192e444af51b3b7dabb
Ebuka Odini <ebukaodini@gmail.com>
2021-10-29 23:01:46 +0100

    updated the ui for the welcome page

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sdride_sample/utils/functions.dart';
 
@@ -12,27 +13,76 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      backgroundColor: Colors.blue,
+      body: Stack(
+        alignment: Alignment.center,
         children: [
-          Text(
-            'Welcome\nRider',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
+          Positioned(
+              top: screen(context).height * 0.3,
+              child: Column(
+                children: [
+                  Icon(
+                    CupertinoIcons.car_detailed,
+                    size: 100,
+                    color: Colors.blue.shade50,
+                  ),
+                  Text(
+                    'SD Ride',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.blue.shade50,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    ),
+                  ),
+                  // Text(
+                  //   'Drivers',
+                  //   textAlign: TextAlign.center,
+                  //   style: TextStyle(
+                  //     color: Colors.blue.shade50,
+                  //     fontWeight: FontWeight.bold,
+                  //     fontSize: 20,
+                  //   ),
+                  // ),
+                ],
+              )),
+          Positioned(
+            // bottom: 60,
+            top: screen(context).height * 0.7,
+            child: SizedBox(
+              width: screen(context).width * 0.8,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.blue.shade50),
+                  foregroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
+                onPressed: () => Navigator.pushNamed(context, '/map'),
+                child: Text('Continue'),
+              ),
             ),
           ),
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/map'),
-            child: Text('Continue'),
-          )
         ],
       ),
-    ));
+      // Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      // Text(
+      //   'Welcome\nRider',
+      //   textAlign: TextAlign.center,
+      //   style: TextStyle(
+      //     color: Colors.blue,
+      //     // fontWeight: FontWeight.bold,
+      //     fontSize: 40,
+      //   ),
+      // ),
+      //       SizedBox(height: 30),
+
+      //     ],
+      //   ),
+      // ),
+    );
   }
 }
